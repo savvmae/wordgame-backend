23a49889f3df60a57cfec008de3839aff6bc509e
Savannah Cosby <savannah.cosby123@gmail.com>
2017-06-23 18:03:03 -0400

    working on win logic, lose logic and reset is working

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 //need to:
 // render length of correct word to game page
-// fill in correct guess with guess
+// multiple letters in correct word - doesn't work currently
+//
+//
+// hard mode:
+// let user pick length of word/difficulty
 // 
 
 const express = require('express');
@@ -39,10 +43,12 @@ application.get('/', (request, response) => {
 application.get('/game', (request, response) => {
 
     request.session.newWord = words[Math.floor(Math.random() * (words.length - 1))];
+    request.session.newWordArr = request.session.newWord.split("");
     request.session.guessCount = 0;
     request.session.correctGuesses = [];
     request.session.incorrectGuesses = [];
     request.session.allGuesses = [];
+    request.session.currentGuess = "";
     var newGame = request.session;
     response.render('game', newGame);
 
@@ -50,49 +56,38 @@ application.get('/game', (request, response) => {
 
 application.post('/game', (request, response) => {
     request.session.error = "";
-    request.session.currentGuess = request.body.guess;
-    
-    if (request.session.currentGuess.length > 1 || request.session.currentGuess.length === 0 ){
+    request.session.currentGuess = request.body.guess.toLowerCase();
+
+    if (request.session.currentGuess.length > 1 || request.session.currentGuess.length === 0) {
         request.session.error = "Invalid guess";
         response.render('game', request.session);
-    } else if (request.session.allGuesses.indexOf(request.session.currentGuess) != -1){
+    } else if (request.session.allGuesses.indexOf(request.session.currentGuess) != -1) {
         request.session.error = "You've already guessed that letter";
         response.render('game', request.session);
     } else {
         request.session.allGuesses.push(request.session.currentGuess);
 
-        response.render('game', request.session);
+
+        if (request.session.newWordArr.indexOf(request.session.currentGuess) != -1) {
+            request.session.correctGuesses.push(request.session.currentGuess);
+            // for (i = 0; i < request.session.newWordArr.length; i++) {
+            //     var index = request.session.newWordArr.indexOf(request.session.currentGuess);
+            //     request.session.newWordArr = request.session.newWordArr.slice(0, index) + request.session.newWordArr.slice(index + 1);
+            // }
+            // this is broken, fix logic
+            response.render('game', request.session);
+        } else {
+            request.session.incorrectGuesses.push(request.session.currentGuess);
+            request.session.guessCount++;
+            if (request.session.guessCount >= 8) {
+                response.render('lost', request.session);
+            } else {
+                response.render('game', request.session);
+            }
+        }
     }
-    
 });
 
 
-
-// application.use((request, response, next) => {
-//     var token = "";
-//     if(request.cookies){
-//         token = request.cookies['token'];
-//     }
-//     var user = {};
-//     if(token){
-//         user.isAuthenticated = true;
-//         user.name = "bob";
-// } else {
-//     user.isAuthenticated = false;
-// }
-//     response.locals.user = user;
-//     next();
-// });
-
-
-
-// application.get('/', (request, response) => {
-//     if(response.locals.user.isAuthenticated){
-//         response.send('Is authenticated, hello - '+ response.locals.user.name);
-//     } else {
-//         response.send("is not authenticated");
-//     }
-   
-// })
 
 application.listen(3000);
